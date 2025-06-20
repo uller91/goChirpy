28fb0887e6191fa7d70993a236784f5d5dd93317
uller91 <girnikis@gmail.com>
2025-06-19 18:39:02 -0700

    server and various handler fucntions added

--- main.go
+++ main.go
@@ -1,5 +1,63 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_ = cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerReq(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
+
+func handlerOk(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+
+
 func main() {
+
+	var counter atomic.Int32
+	counter.Store(0)
+	apiCfg := &apiConfig{fileserverHits: counter,}
+	mux := http.NewServeMux()
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/healthz", handlerOk)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerReq)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	
+	s := http.Server{
+		Addr: ":8080", 
+		Handler: mux,
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	return
 }
